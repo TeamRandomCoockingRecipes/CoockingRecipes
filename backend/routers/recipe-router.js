8f14d0a71e9aa658f455d668f48309a7401cfa49
Todor <todor_ia@abv.bg>
2016-12-28 17:36:38 +0200

    recipes added with the corresponding server changes

--- backend/routers/recipe-router.js
+++ backend/routers/recipe-router.js
@@ -9,17 +9,23 @@ module.exports = function({ app, data }) {
 
     let router = new Router();
 
-    router
-        .get("/create", controller.getCreateRecipeForm)
-        .get("/edit/:id", controller.getEditRecipeForm)
-        .get("/", controller.getAllRecipes)
-        .get("/newest", controller.getNewestRecipesAjax)
-        .get("/:id", controller.getRecipeDetails)
-        .post("/", controller.createRecipe)
-        .post("/edit/:id", controller.editRecipeById)
-        .post("/:id", controller.addComment);
+    // router
+    //     .get("/create", controller.getCreateRecipeForm)
+    //     .get("/edit/:id", controller.getEditRecipeForm)
+    //     .get("/", controller.getAllRecipes)
+    //     .get("/newest", controller.getNewestRecipesAjax)
+    //     .get("/:id", controller.getRecipeDetails)
+    //     .post("/", controller.createRecipe)
+    //     .post("/edit/:id", controller.editRecipeById)
+    //     .post("/:id", controller.addComment);
 
-    app.use("/recipes", router);
+    // app.use("/recipes", router);
+
+    router
+        .get("/", controller.getAllRecipes)
+        .get("/:id", controller.getRecipeDetails);
+
+    app.use("/api/recipes", router);
 
     return router;
 };
