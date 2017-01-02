73e9159c450f4178d5c12df54808d289c4457bfc
JesusLoovesYou <vankodrift1987@abv.bg>
2017-01-02 18:12:14 +0200

    create articles small list component Add it to home component Create hover animation and add it to home article small list

--- public/src/app/home/home.component.ts
+++ public/src/app/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IArticle } from '../article/article';
+import { IRecipe } from '../recipe/recipe';
+
+import { ArticleService } from '../article/services/article.service';
+
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private articles: IArticle[];
+  private recipes: IRecipe[];
 
-  constructor() { }
+  private errorMessage: string;
+
+  constructor(private articleService: ArticleService) { }
 
   ngOnInit() {
+    
   }
 
 }
